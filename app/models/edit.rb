0612e0b3a810bb11c2e8de330bb1969d27fb5de7
Martin Nash <martin.j.nash@gmail.com>
2014-02-23 17:53:59 -0800

    added creator name to entry/show view

--- app/models/edit.rb
+++ app/models/edit.rb
@@ -1,4 +1,9 @@
 class Edit < ActiveRecord::Base
   belongs_to :user
   belongs_to :entry
+
+  scope :creations, -> { where(category: "Create") }
+  scope :udpates,   -> { where(category: "Update") }
+
+
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,18 @@ class Entry < ActiveRecord::Base
   has_many :edits
   has_many :editors, through: :edits, source: :user
 
+
+
+  def creation_edit
+    self.edits.creations.first
+  end
+
+  def creator
+    creation_edit.user if creation_edit
+  end
+
+  def creator_handle
+    creator.handle if creator
+  end
+
 end
